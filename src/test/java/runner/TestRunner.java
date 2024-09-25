701abed9db3cbf0c83d0d0da543506c5e35abbe6
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-09-24 21:06:48 -0400

    added regression and maven plugin and surefile and build

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,6 +9,7 @@ public class TestRunner {
     Karate runTest(){
         return Karate.run("classpath:features")
                 .karateEnv("dev")
-                .tags("Regression");
+                .tags("Regression")
+                .outputCucumberJson(true);
     }
 }
